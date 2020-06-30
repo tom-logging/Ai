9df4a7014ba3180642e11d8240d1200757d126e6
tom <67664316+tom-logging@users.noreply.github.com>
2020-07-01 00:26:29 +0100

    Update embeds.py

--- cogs/embeds.py
+++ cogs/embeds.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python
 import discord
 import datetime
-from datetime import timezone, tzinfo, timedelta
+from datetime import datime
 from discord.utils import get
 from discord.ext.commands import Bot
 from discord.ext import commands, tasks
